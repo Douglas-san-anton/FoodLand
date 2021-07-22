3a5dc848d951b8ac022009da558a4d6e8cf54b87
Douglas-san-anton <73114495+Douglas-san-anton@users.noreply.github.com>
2021-07-21 23:44:19 -0300

    Terminado los test

--- api/tests/models/recipe.spec.js
+++ api/tests/models/recipe.spec.js
@@ -2,20 +2,53 @@ const { Recipe, conn } = require('../../src/db.js');
 const { expect } = require('chai');
 
 describe('Recipe model', () => {
-  before(() => conn.authenticate()
-    .catch((err) => {
+  before(() =>
+    conn.authenticate().catch((err) => {
       console.error('Unable to connect to the database:', err);
-    }));
+    })
+  );
   describe('Validators', () => {
     beforeEach(() => Recipe.sync({ force: true }));
-    describe('name', () => {
-      it('should throw an error if name is null', (done) => {
+    describe('title', () => {
+      it('should throw an error if title is null', (done) => {
         Recipe.create({})
-          .then(() => done(new Error('It requires a valid name')))
+          .then(() => done(new Error('It requires a valid title')))
           .catch(() => done());
       });
-      it('should work when its a valid name', () => {
-        Recipe.create({ name: 'Milanesa a la napolitana' });
+      it('should work when its a valid title', () => {
+        Recipe.create({
+          title: 'Salsa criolla',
+          summary: 'Es hermana del chimichurri',
+          spoonacularScore: 100,
+          healthScore: 50,
+          analyzedInstructions:
+            'Cortas cebollita, tomate y pimiento',
+        });
+      });
+
+      it('should receive an object for recipe', () => {
+        let recipe = {
+          title: 'Salsa criolla',
+          summary: 'Es hermana del chimichurri',
+          spoonacularScore: 100,
+          healthScore: 50,
+          analyzedInstructions:
+            'Cortas cebollita, tomate y pimiento.'
+        };
+        expect(recipe).to.be.a('object');
+      });
+      it('should receive a number in both score properties', () => {
+        let recipe = {
+          title: 'Salsa criolla',
+          summary: 'Es hermana del chimichurri',
+          spoonacularScore: 'no te paso score',
+          healthScore: 'menos esto',
+          analyzedInstructions:
+            'Cortas cebollita, tomate y pimiento'
+        };
+        expect(recipe).to.be.a('object');
+        expect(typeof recipe.spoonacularScore === 'number').to.equal(false);
+        expect(typeof recipe.healthScore === 'number').to.equal(false);
       });
     });
   });
--- api/tests/routes/country.spec.js
+++ api/tests/routes/country.spec.js
@@ -6,19 +6,44 @@ const { Recipe, conn } = require('../../src/db.js');
 
 const agent = session(app);
 const recipe = {
-  name: 'Milanea a la napolitana',
+  title: 'Milanesa a la napolitana',
+  summary: 'lo mas rico que hay'
 };
 
-describe('Recipe routes', () => {
+ describe('Recipe routes', () => {
   before(() => conn.authenticate()
   .catch((err) => {
     console.error('Unable to connect to the database:', err);
   }));
-  beforeEach(() => Recipe.sync({ force: true })
+  beforeEach(() => Recipe.sync({force: true})
     .then(() => Recipe.create(recipe)));
-  describe('GET /recipes', () => {
-    it('should get 200', () =>
-      agent.get('/recipes').expect(200)
-    );
-  });
+ describe('GET /recipes', () => {
+      it('responds with 200', async () => {
+        try {
+          await agent.get('/recipes').expect(200);
+        } catch (err) {
+          console.log(err);
+        }
+      }).timeout(10000);
+      it('responds an array with 101 recipes', async () => {
+        try {
+          const res = await agent.get('/recipes');
+          expect(res.body).to.have.lengthOf(101);
+        } catch (err) {
+          console.log(err);
+        }
+      }).timeout(10000);
+      it('If the title query is passed, the recipe should match with that title', async () => {
+        try {
+          const res = await agent.get('/recipes?title=pasta');
+          expect(res.body[0].title).to.be.equal('pasta');
+        } catch (err) {}
+      }).timeout(10000);
+      it('If an id parameter is passed it must return the recipe associated with that id', async () => {
+        try {
+          const res = await agent.get('/recipes/716381');
+          expect(res.body[0].title).to.be.equal('Nigerian Snail Stew');
+        } catch (err) {}
+      }).timeout(10000);
+    });
 });
